9424ee23ae650947c3887ee153f6ebacab0ac620
Walter Cervini <wcervini@yiredmedia.com>
2024-02-11 20:50:49 +0000

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
+// import storyblok from '@storyblok/astro';
+// import { loadEnv } from 'vite';
+
+// const env = loadEnv("accessToken", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({
+    // integrations: [
+    //     storyblok({
+    //       accessToken: env.STORYBLOK_TOKEN,
+    //       components: {
+    //         // Add your components here
+    //       },
+    //       apiOptions: {
+    //         // Choose your Storyblok space region
+    //         region: 'us', // optional,  or 'eu' (default)
+    //       },
+    //     })
+    //   ],
+// });
